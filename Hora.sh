ee9da91ba6a0ca8325f5052d3fa8c137d8d609af
tele <ana.giraldo5@udea.edu.co>
2020-11-26 08:47:02 -0500

    Hora V3

--- Hora.sh
+++ Hora.sh
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 #!/bin/bash
 #Ana Maria Giraldo
 echo Mi N-esimo repositorio inicio a
+=======
+echo Mi primer repositorio inicio a
+>>>>>>> Hora V3
 date +%H:%M
 echo IP a hacer ping:
 read direccion
